bb24be5cb35c1be85a0384b4df69a259dd76685a
Asadbek-IP <dasturchi6678@gmail.com>
2021-05-05 09:11:12 +0500

    debug show hidden

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,6 @@ import 'package:flutter/widgets.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  theme: 
-  ThemeData(primaryColor: Colors.red, accentColor: Colors.yellowAccent),
   debugShowCheckedModeBanner: false,
   home: SplashScreen(),
 ));
