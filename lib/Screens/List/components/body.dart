0c8459e9cb5682a212588f0aa4fcacaa41086fa5
Asadbek-IP <dasturchi6678@gmail.com>
2021-05-09 18:04:57 +0500

    search bar

--- lib/Screens/List/components/body.dart
+++ lib/Screens/List/components/body.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:debtors/constants.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class Body extends StatefulWidget {
   @override
@@ -10,11 +11,45 @@ class _BodyState extends State<Body> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      body: Container(
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [Image.asset("assets/images/background.png")])),
       appBar: AppBar(
+        leading: IconButton(
+          onPressed: () {},
+          icon: Icon(
+            Icons.account_circle_outlined,
+            color: Colors.white,
+            size: 35,
+          ),
+        ),
         backgroundColor: kPrimaryColor,
-        centerTitle: true,
+        titleSpacing: 20,
+        actions: <Widget>[
+          IconButton(
+            onPressed: () {},
+            icon: Icon(
+              Icons.search,
+              color: Colors.white,
+              size: 28,
+            ),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: Icon(
+              Icons.more_vert,
+              color: Colors.white,
+              size: 28,
+            ),
+          ),
+        ],
         title: Text(
-          'Qarzdorlar',
+          'Qarzdorlar ro\'yxati',
+          textAlign: TextAlign.left,
+          style: TextStyle(
+            color: Colors.white,
+          ),
         ),
       ),
     );
